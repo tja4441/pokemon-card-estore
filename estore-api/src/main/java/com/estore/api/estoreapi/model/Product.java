4a50e9348d8a3c2966ac0328f5ac9bd1f971f90b
Gabriel <gabrielbuxo1@gmail.com>
2022-09-22 19:50:47 -0400

    Actually really for real finished implementing the whole Product class. for real

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 public class Product {
     static int ID = 0;
     static HashMap<Integer, Float> PriceMap;
+    static HashMap<Integer, Integer> QuantityMap;
     private String name;
     private int quantity;
     private int id;
@@ -38,17 +39,19 @@ public class Product {
      * a product from an existing product(from the inventory)
      * @param product an existing product(from the inventory)
      * @param amount the amount of product that you are "taking" from the other product
+     * @throws Error Throws an error when amount of product requested exceeds amount of product
      */
     public Product(Product product, int amount) throws Error {
-        if (amount > product.quantity) {
+        if (amount > QuantityMap.get(product.id)) {
             throw new Error("Amount demanded is greater than amount of product");
         }
         this.id = product.id;
         this.name = product.name;
+        this.quantity = amount;
     }
 
     /**
-     * sets Project classes ID to a number(from persistant data)
+     * sets Product class' ID to a number(from persistant data)
      * @param id the id of the next
      */
     public static void setID(int id){
@@ -60,6 +63,37 @@ public class Product {
      */
     public static int getID(){
         return ID;
+    }
+
+    /**
+     * @return returns the price of this product
+     */
+    static float getQuantity(int id) {
+        return QuantityMap.get(id);
+    }
+    
+    /**
+     * @param price Sets the price of this product to this value
+     */
+    static void setQuantity(int id, int amount) {
+        QuantityMap.put(id, amount);
+    }
+    
+    /**
+     * gets the price of  a product
+     * @param id the id of the product you would like to get the price of
+     * @return returns the price of the product with this id
+     */
+    static float getPrice(int id) {
+        return PriceMap.get(id);
+    }
+    
+    /**
+     * @param id the id of the product you would like to change the price of
+     * @param price Sets the price of this product to this value
+     */
+    static void setPrice(int id, Float price) {
+        PriceMap.put(id, price);
     }
 
     /**
@@ -109,5 +143,35 @@ public class Product {
      */
     public void changeQuantity(int quantity) {
         this.quantity += quantity;
+    }
+
+    /**
+     * returns a string representation of the product
+     */
+    @Override
+    public String toString(){
+        StringBuilder output = new StringBuilder("{");
+        output.append("\tid: " + this.id + '\n');
+        output.append("\tname: " + this.name + '\n');
+        output.append("\tquantity: " + this.quantity + '\n');
+        output.append("\tprice: " + PriceMap.get(this.id) + "\n}");
+        return output.toString();
+    }
+
+    /**
+     * returns true if 2 Products have the same ID
+     */
+    @Override
+    public boolean equals(Object other){
+        if (!(other instanceof Product)) {
+            return false;
+        }
+        Product otherProduct = (Product) other;
+        if(this.id == otherProduct.id) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
