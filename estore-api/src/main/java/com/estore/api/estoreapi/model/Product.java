3295cb88f061c946fc1eb712649bebde257966b7
Gabriel <gabrielbuxo1@gmail.com>
2022-09-22 18:17:58 -0400

    Implemented Product class(Missing documentation)

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
@@ -1,5 +1,40 @@
 package com.estore.api.estoreapi.model;
 
 public class Product {
+    static int ID = 0;
+    private String name;
+    private int quantity;
+    private int id;
+    private float price;
     
+    public Product(String name, int quantity, float price) {
+        this.id = ID++;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
