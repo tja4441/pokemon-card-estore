6a9e5c65a0b7f4ce14666e43f952d00fedd8e14f
zach <zw7571@g.rit.edu>
2022-09-23 17:27:04 -0400

    Updated the File Dao for get products

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
@@ -1,5 +1,17 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.Product;
+
 public interface InventoryDao {
+
+    /**
+     * Retrieves a product with the given name
+     * @param name The name of the product to get
+     * @return a product object with the matching name
+     * @throws IOException if an issue with storage
+     */
+    Product getProduct(String name) throws IOException;
     
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
@@ -1,5 +1,25 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+import java.util.Map;
+
+import com.estore.api.estoreapi.model.Product;
+
 public class InventoryFileDao implements InventoryDao {
+
+    Map<String, Product> products;
+
+    /**
+     ** {@inheritDoc}
+     */
+    @Override
+    public Product getProduct(String name) throws IOException {
+        synchronized(products){
+            if(products.containsKey(name))
+                return products.get(name);
+            else
+                return null;
+        }   
+    }
     
 }
