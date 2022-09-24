4d75adc58dec32eab3d40f3e6e1fb874183fcea8
tja4441 <tja4441@g.rit.edu>
2022-09-24 18:24:02 -0400

    Everything compiles, but I don't think it's working correctly. Taking a break, will continue in a while.

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -1,5 +1,44 @@
 package com.estore.api.estoreapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.estore.api.estoreapi.persistence.InventoryDao;
+import com.estore.api.estoreapi.model.Product;
+
 public class InventoryController extends Controller {
-    
+    private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
+    private InventoryDao inventoryDao;
+
+    /**
+     * Updates the {@linkplain Product product} with the provided {@linkplain Product product} object, if it exists
+     * 
+     * @param hero The {@link Product product} to update
+     * 
+     * @return ResponseEntity with updated {@link Product product} object and HTTP status of OK if updated<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        LOG.info("PUT /products " + product);
+
+        try {
+            Product p = inventoryDao.updateProduct(product);
+            if (p != null)
+            return new ResponseEntity<Product>(p,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    catch(IOException e) {
+        LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    }
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
@@ -1,5 +1,24 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+import com.estore.api.estoreapi.model.Product;
+
+/**
+ * Defines the interface for Hero object persistence
+ * 
+ * @author Timothy Avila
+ */
 public interface InventoryDao {
-    
+    /**
+     * Updates and saves a {@linkplain Product product}
+     * 
+     * @param {@link Product product} object to be updated and saved
+     * 
+     * @return updated {@link Product product} if successful, null if
+     * {@link Product product} could not be found
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    Product updateProduct(Product product) throws IOException;
 }
+
