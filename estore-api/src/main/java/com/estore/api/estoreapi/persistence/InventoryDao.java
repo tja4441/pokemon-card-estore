bdf50e55c94594cbdf517124acd7202361d7308c
zach <zw7571@g.rit.edu>
2022-09-23 22:31:29 -0400

    Updated the DAO to get a list of products

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
@@ -1,5 +1,17 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.estore.api.estoreapi.model.Product;
+
 public interface InventoryDao {
     
+    /**
+     * Retrieves all products in the inventory
+     * @return An array of product objects, may be empty
+     * @throws IOException if an issue with storage
+     */
+    ArrayList<Product> getProducts() throws IOException;
+
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
@@ -1,5 +1,25 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.estore.api.estoreapi.model.Product;
+
 public class InventoryFileDao implements InventoryDao {
-    
+
+    Map<Integer, Product> products;
+
+    /**
+     * *{@inheritDoc}
+     */
+    @Override
+    public ArrayList<Product> getProducts() throws IOException {
+        ArrayList<Product> productsList = new ArrayList<>();
+        for(Product p : products.values()){
+            productsList.add(p);
+        }
+        return productsList;
+    }
+  
 }
