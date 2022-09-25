495c41f0037950a43e1ae71360781405335e786b
dep7351 <dep7351@g.rit.edu>
2022-09-25 11:32:36 -0400

    Added method stub with documentation to the InventoryDAO interface that will allow for the deletion of a product - Daniel Pittman

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
@@ -1,5 +1,19 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.Product;
+
 public interface InventoryDao {
     
+    /**
+     * Deletes a {@linkplain Product product} when given a product id
+     * 
+     * @param id the internal {@link Product product} identifier
+     * 
+     * @return true if {@link Product product} was deleted, false if product doesn't exist
+     * 
+     * @throws IOException if system encounters any situation that inhibits deletion
+     */
+    boolean deleteProduct(int id) throws IOException;
 }
