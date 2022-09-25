2c5e50b4bfc97220535dc2fdf140db5b228bc480
jderosier <jld3877@rit.edu>
2022-09-25 09:40:14 -0400

    implemented create product

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDao.java
@@ -1,5 +1,45 @@
 package com.estore.api.estoreapi.persistence;
 
+import com.estore.api.estoreapi.model.Product;
+import java.io.IOException;
+
+/**
+ * Defines the interface mediating Product persistance
+ * 
+ * @author Team Engenuity
+ */
 public interface InventoryDao {
     
+    /**
+     * Retreives a Product From storage with the given ID
+     * 
+     * @param id the id of the Product to get
+     * 
+     * @return A Product with the matching ID, null if no Product found
+     * 
+     * @throws IOException
+     */
+    Product getProduct(int id) throws IOException;
+
+    /**
+     * Saves the current State of the map to a file
+     * 
+     * @return If the objects were properly saved
+     * 
+     * @throws IOException
+     */
+    Boolean save() throws IOException;
+
+    /**
+     * Creates and Stores product
+     * 
+     * @param product
+     * 
+     * @return The Product that was created
+     * 
+     * @throws IOException
+     */
+    Product createProduct(Product product) throws IOException;
+
+
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
@@ -1,5 +1,77 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import com.estore.api.estoreapi.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class InventoryFileDao implements InventoryDao {
-    
+    Map<Integer, Product> products;
+    private ObjectMapper objectMapper;
+    private String filename;
+
+    /**
+     * Creates a Inventory File Dao
+     * 
+     * @param filename Filename to Read and Write
+     * @param objectMapper Provides JSON <-> Java Object
+     */
+    public InventoryFileDao(String filename, ObjectMapper objectMapper) {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * 
+     */
+    public Product[] getProductsArray() {
+        ArrayList<Product> productList = new ArrayList<>();
+
+        for(Product Product : products.values()) {
+            productList.add(Product);
+        }
+        
+        Product[] productArray = new Product[productList.size()];
+        productList.toArray(productArray);
+        return productArray;
+    }
+
+    /**
+     * 
+     * @return True if the objects were properly saved
+     */
+    public Boolean save(){
+        Product[] productArray = getProductsArray();
+
+        try {
+            objectMapper.writeValue(new File(filename), productArray);
+            return true;
+        }
+        catch(IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Product getProduct(int id) throws IOException {
+        synchronized(products) {
+            if(products.containsKey(id)) {
+                return products.get(id);
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public Product createProduct(Product product) throws IOException {
+        synchronized(products) {
+            products.put(product.getId(), product);
+            save();
+            return product;
+        }
+    }
 }
