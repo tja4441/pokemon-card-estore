18c0f2d41373706f01fcfb958f5f2296ca268aee
zach <zw7571@g.rit.edu>
2022-09-25 14:43:37 -0400

    Hotfixed the merge

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
@@ -6,20 +6,11 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.ArrayList;
 
-import com.estore.api.estoreapi.model.Product;
-
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.logging.Logger;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.springframework.beans.factory.annotation.Value;
-
-import com.estore.api.estoreapi.model.Product;
 
 public class InventoryFileDao implements InventoryDao {
 
@@ -55,38 +46,6 @@ public class InventoryFileDao implements InventoryDao {
     }
 
     /**
-     * Generates an array of {@linkplain Product products} from the tree map
-     * 
-     * @return  The array of {@link Product products}, may be empty
-     */
-    private Product[] getProductsArray() {
-        return getProductsArray(null);
-    }
-
-    /**
-     * Generates an array of {@linkplain Product product} from the tree map for any
-     * {@linkplain Product products} that contains the text specified by containsText
-     * <br>
-     * If containsText is null, the array contains all of the {@linkplain Product products}
-     * in the tree map
-     * 
-     * @return  The array of {@link Product products}, may be empty
-     */
-    private Product[] getProductsArray(String containsText) { // if containsText == null, no filter
-        ArrayList<Product> productArrayList = new ArrayList<>();
-
-        for (Product product : products.values()) {
-            if (containsText == null || product.getName().contains(containsText)) {
-                productArrayList.add(product);
-            }
-        }
-
-        Product[] productArray = new Product[productArrayList.size()];
-        productArrayList.toArray(productArray);
-        return productArray;
-    }
-
-    /**
      * Saves the {@linkplain Product products} from the map into the file as an array of JSON objects
      * 
      * @return true if the {@link Product products} were written successfully
@@ -131,30 +90,8 @@ public class InventoryFileDao implements InventoryDao {
         ++nextId;
         return true;
     }
-        /**
-     * Creates a Product File Data Access Object
-     * 
-     * @param filename Filename to read from and write to
-     * @param objectMapper Provides JSON Object to/from Java Object serialization and deserialization
-     * 
-     * @throws IOException when file cannot be accessed or read from
-     */
-    public InventoryFileDao(@Value("${inventory.file}") String filename,ObjectMapper objectMapper) throws IOException {
-        this.filename = filename;
-        this.objectMapper = objectMapper;
-        load();  // load the products from the file
-    }
 
-    /**
-     * Generates the next id for a new {@linkplain Product product}
-     * 
-     * @return The next id
-     */
-    private synchronized static int nextId() {
-        int id = nextId;
-        ++nextId;
-        return id;
-    }
+   
 
     /**
      * Generates an array of {@linkplain Product products} from the tree map
@@ -187,54 +124,6 @@ public class InventoryFileDao implements InventoryDao {
         productArrayList.toArray(productArray);
         return productArray;
     }
-
-    /**
-     * Saves the {@linkplain Product products} from the map into the file as an array of JSON objects
-     * 
-     * @return true if the {@link Product products} were written successfully
-     * 
-     * @throws IOException when file cannot be accessed or written to
-     */
-    private boolean save() throws IOException {
-        Product[] productArray = getProductsArray();
-
-        // Serializes the Java Objects to JSON objects into the file
-        // writeValue will thrown an IOException if there is an issue
-        // with the file or reading from the file
-        objectMapper.writeValue(new File(filename),productArray);
-        return true;
-    }
-
-    /**
-     * Loads {@linkplain Hero heroes} from the JSON file into the map
-     * <br>
-     * Also sets next id to one more than the greatest id found in the file
-     * 
-     * @return true if the file was read successfully
-     * 
-     * @throws IOException when file cannot be accessed or read from
-     */
-    private boolean load() throws IOException {
-        products = new TreeMap<>();
-        nextId = 0;
-
-        // Deserializes the JSON objects from the file into an array of heroes
-        // readValue will throw an IOException if there's an issue with the file
-        // or reading from the file
-        Product[] productArray = objectMapper.readValue(new File(filename),Product[].class);
-
-        // Add each hero to the tree map and keep track of the greatest id
-        for (Product product : productArray) {
-            products.put(product.getId(),product);
-            if (product.getId() > nextId)
-                nextId = product.getId();
-        }
-        // Make the next id one greater than the maximum from the file
-        ++nextId;
-        return true;
-    }
-    
-    
 
     /**
      * *{@inheritDoc}
@@ -276,17 +165,6 @@ public class InventoryFileDao implements InventoryDao {
                 return null;
             }               
         }
-    
-    /**
-     * Creates a Inventory File Dao
-     * 
-     * @param filename Filename to Read and Write
-     * @param objectMapper Provides JSON <-> Java Object
-     */
-    public InventoryFileDao(String filename, ObjectMapper objectMapper) {
-        this.filename = filename;
-        this.objectMapper = objectMapper;
-    }
 
     @Override
     public Product createProduct(Product product) throws IOException {
