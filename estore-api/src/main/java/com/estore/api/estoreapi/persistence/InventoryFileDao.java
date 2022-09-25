f1c016e54c1705ba831be527f7042502a66443f9
dep7351 <dep7351@g.rit.edu>
2022-09-25 12:21:08 -0400

    Added the deleteProduct and other dependent methods - save, getProductsArray(), and getProductsArray(String containsText) - to the InventoryFileDao class that implements InventoryDao - Daniel Pittman

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDao.java
@@ -1,5 +1,83 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.estore.api.estoreapi.model.Product;
+
 public class InventoryFileDao implements InventoryDao {
+
+    Map<Integer,Product> products;      // provides a local cache of the product objects
+    ObjectMapper objectMapper;          //Converts between Hero objects and JSON text file formats
+    String filename;                    //Filename to read/write
+
+    /**
+     * Generates an array of {@linkplain Product products} from the tree map
+     * 
+     * @return  The array of {@link Product products}, may be empty
+     */
+    private Product[] getProductsArray() {
+        return getProductsArray(null);
+    }
+
+    /**
+     * Generates an array of {@linkplain Product products} from the tree map for any
+     * {@linkplain Product products} that contains the text specified by containsText
+     * 
+     * If containsText is null, the array contains all of the {@linkplain Product products}
+     * in the tree map
+     * 
+     * @return  The array of {@link Product products}, may be empty
+     */
+    private Product[] getProductsArray(String containsText) { // if containsText == null, no filter
+        ArrayList<Product> productArrayList = new ArrayList<>();
+
+        for (Product product : products.values()) {
+            if (containsText == null || product.getName().contains(containsText)) {
+                productArrayList.add(product);
+            }
+        }
+
+        Product[] productArray = new Product[productArrayList.size()];
+        productArrayList.toArray(productArray);
+        return productArray;
+    }
+
+    /**
+     * Saves the {@linkplain Product products} from the map into the file as an array of JSON objects
+     * 
+     * @return true if the {@link Product products} were written successfully
+     * 
+     * @throws IOException when file cannot be accessed or written to
+     */
+    private boolean save() throws IOException {
+        Product[] heroArray = getProductsArray();
+
+        // Serializes the Java Objects to JSON objects into the file
+        // writeValue will thrown an IOException if there is an issue
+        // with the file or reading from the file
+        objectMapper.writeValue(new File(filename),heroArray);
+        return true;
+    }
+
+    /**
+     ** {@inheritDoc}
+     */
+    @Override
+    public boolean deleteProduct(int id) throws IOException {
+        synchronized(products) {
+            if(products.containsKey(id)){
+                products.remove(id);
+                return save();
+            }
+            else{
+                return false;
+            }
+        }
+    }
     
 }
