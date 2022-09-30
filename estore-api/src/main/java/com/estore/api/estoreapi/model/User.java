fc353d492bcc951774657514b7cbea3e29b26914
zach <zw7571@g.rit.edu>
2022-09-30 01:04:51 -0400

    Started implementation of the user class

--- estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
@@ -1,5 +1,41 @@
 package com.estore.api.estoreapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
+
+    @JsonProperty("id") private int id;
+    @JsonProperty("UserName") private String userName;
+    @JsonProperty("Type") private String type;
     
+    
+    public User( @JsonProperty("id") int id, 
+                    @JsonProperty("UserName") String userName, 
+                    @JsonProperty("Type") String type  ){
+
+        this.id = id;
+        this.userName = userName;
+        this.type = type;
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+
+
+
 }
