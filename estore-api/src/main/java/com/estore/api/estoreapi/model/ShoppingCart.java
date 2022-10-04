22c6117c32a55802b89c05d3fbd52160397db917
jderosier <jld3877@rit.edu>
2022-10-04 08:46:54 -0400

    added documentation

--- estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
@@ -2,24 +2,66 @@ package com.estore.api.estoreapi.model;
 
 import java.util.HashSet;
 
+    /**
+     * Class Representing a Shopping Cart
+     * 
+     * @author Team E
+     */
 public class ShoppingCart {
     private HashSet<Product> cartSet;
 
+    /**
+     * Constructor with no Given Set, Returns Empty Shopping Cart
+     */
     public ShoppingCart() {
         this(new HashSet<Product>());
     }
+
+    /**
+     * Constructor with Given Set, Returns Shopping Cart with items from Set
+     * 
+     * @param cartSet
+     */
     public ShoppingCart(HashSet<Product> cartSet) {
         this.cartSet = cartSet;
     }
 
+    /**
+     * Adds Product to Cart
+     * 
+     * @param product
+     */
     public void addToCart(Product product) {
         this.cartSet.add(product);
     }
+
+    /**
+     * Removes Product from Cart
+     * 
+     * @param product
+     */
     public void removeFromCart(Product product) {
         this.cartSet.remove(product);
     }
 
+    /**
+     * Gets Num of Products of Cart
+     * 
+     * @return Num Items in Cart
+     */
     public int size() {
         return this.cartSet.size();
+    }
+
+    //TODO: Checkout Method
+
+    /**
+     * Formats Shopping Cart as a String
+     * 
+     * @return String format of Shopping Cart
+     */
+    @Override
+    public String toString() {
+        return this.cartSet.toString();
     }
 }
