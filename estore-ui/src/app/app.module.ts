6b5fdfb8f6847fdc3ae6133304361ef23154d547
tja4441 <tja4441@g.rit.edu>
2022-10-11 16:26:27 -0400

    Adding functionality for Admin to add a product through the UI. None of this works, but it all compiles, and I can't figure out what's wrong

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
