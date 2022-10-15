0959c90a9624bf85eb4b1fa81bda6df50c44772c
dep7351 <dep7351@g.rit.edu>
2022-10-15 19:28:02 -0400

    I have created a ShoppingCartDao interface for the persistence tier with all the proper comments. I did not put all of the methods in the interface for shopping cart as we did for product because some of the methods wouldn't make sense for a shopping cart. More to follow. - Daniel Pittman

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartDao.java
@@ -1,5 +1,66 @@
 package com.estore.api.estoreapi.persistence;
 
-public class ShoppingCartDao {
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.ShoppingCart;
+
+/**
+ * Defines the interface mediating Shopping Cart persistance
+ * 
+ * @author Daniel Pittman
+ */
+public interface ShoppingCartDao {
     
+    /**
+     * Updates and saves a {@linkplain ShoppingCart cart}
+     * 
+     * @param {@link Shoppingcart cart} object to be updated and saved
+     * 
+     * @return updated {@link ShoppingCart cart} if successful, null if otherwise
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     * 
+     * @author Daniel Pittman
+     */
+    ShoppingCart updateCart(ShoppingCart cart) throws IOException;
+
+    /**
+     * Retrieves a {@linkplain ShoppingCart cart} with the given id
+     * 
+     * @param id the id of the {@link ShoppingCart cart} to get
+     * 
+     * @return a {@link ShoppingCart cart} object with the matching id
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     * 
+     * @author Daniel Pittman
+     */
+    ShoppingCart getShoppingCart(int id) throws IOException;
+
+    /**
+     * Creates and stores {@linkplain Shoppingcart cart}
+     * 
+     * @param {@link ShoppingCart cart} object being created 
+     * 
+     * @return the {@link ShoppingCart cart} that was created
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     * 
+     * @author Daniel Pittman
+     */
+    ShoppingCart createShoppingCart(ShoppingCart cart) throws IOException;
+
+    /**
+     * Deletes an {@linkplain ShoppingCart cart} given an id
+     * 
+     * @param id the internal {@link ShoppingCart cart} identifier
+     * 
+     * @return true if {@link ShoppingCart cart} was deleted, false otherwise
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     * 
+     * @author Daniel Pittman
+     */
+    boolean deleteShoppingCart(int id) throws IOException;
+
 }
