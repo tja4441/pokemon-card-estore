b74214ea21c3e637ea9084fc11df5b0b08a961f0
dep7351 <dep7351@g.rit.edu>
2022-10-16 09:54:55 -0400

    I have implemented the shoppingCartController and am now about to make any connections needed to make the architecture work fully. - Daniel Pittman

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ShoppingCartController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ShoppingCartController.java
@@ -1,9 +1,15 @@
 package com.estore.api.estoreapi.controller;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,20 +35,111 @@ public class ShoppingCartController extends Controller{
         this.inventoryController = inventoryController;
     }
 
+    /**
+     * Deletes a {@linkplain ShoppingCart cart} with the given id
+     * 
+     * @param id The id of the {@link ShoppingCart cart} to delete
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted, NOT_FOUND if not found, and INTERNAL_SERVER_ERROR otherwise
+     * 
+     * @author Daniel Pittman
+     */
+    @DeleteMapping("/{id}")
     public ResponseEntity<ShoppingCart> deleteCart(@PathVariable int id) {
-        
+        LOG.info("DELETE /ShoppingCarts/" + id);
+
+        try {
+            Boolean cartDeleted = shoppingCartDao.deleteCart(id);
+            if (cartDeleted != false) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    /**
+     * Updates the {@linkplain ShoppingCart cart} with the provided {@link ShoppingCart cart} object, if it exists
+     * 
+     * @param shoppingCart The {@link ShoppingCart cart} to update
+     * 
+     * @return ResponseEntity with updated {@link ShoppingCart cart} object and HTTP status of OK if updated,
+     *         ResponseEntity with HTTP status of CONFLICT if not found or update not valid,
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * 
+     * @author Daniel Pittman
+     */
+    @PutMapping("")
     public ResponseEntity<ShoppingCart> updateCart(@RequestBody ShoppingCart shoppingCart) {
-
+        LOG.info("PUT /ShoppingCarts " + shoppingCart);
+        try {
+            ShoppingCart updatedCart = shoppingCartDao.updateCart(shoppingCart);
+            if (p != null) {
+                return new ResponseEntity<ShoppingCart>(updatedCart,HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    /**
+     * Responds to the GET request for a {@linkplain ShoppingCart cart} given id
+     * 
+     * @param id The id of the {@link ShoppingCart cart} to get
+     * 
+     * @return ResponseEntity with updated {@link ShoppingCart cart} object and HTTP status of OK if found,
+     *         ResponseEntity with HTTP status of CONFLICT if not found,
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * 
+     * @author Daniel Pittman
+     */
+    @GetMapping("/{id}")
     public ResponseEntity<ShoppingCart> getCart(@PathVariable int id) {
-
+        LOG.info("GET /ShoppingCarts/" + id);
+        try {
+            ShoppingCart cart = shoppingCartDao.getCart(id);
+            if (cart != null) {
+                return new ResponseEntity<ShoppingCart>(cart,HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<ShoppingCart> createProduct(@RequestBody ShoppingCart cart) {
-
+    /**
+     * Creates a {@linkplain ShoppingCart cart} with the provided {@link ShoppingCart cart}
+     * 
+     * @param cart The {@link ShoppingCart cart} to create
+     * 
+     * @return ResponseEntity with created {@link ShoppingCart cart} object and HTTP status of CREATED if cart created,
+     *         ResponseEntity with HTTP status of CONFLICT if {@link ShoppingCart cart} object already exists,
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * 
+     * @author Daniel Pittman
+     */
+    @PostMapping("")
+    public ResponseEntity<ShoppingCart> createCart(@RequestBody ShoppingCart cart) {
+        LOG.info("POST /ShoppingCarts " + cart);
+        try {
+            ShoppingCart newCart = shoppingCartDao.createCart(cart);
+            if (newCart != null) {
+                return new ResponseEntity<ShoppingCart>(newCart,HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
