b7ccfbdccc8ea417b8ce25b00bd9eaa5cf2b8ec7
zach <zw7571@g.rit.edu>
2022-10-17 21:20:54 -0400

    Set up the shoppingCartController class for mocks

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingCartControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingCartControllerTest.java
@@ -1,5 +1,31 @@
 package com.estore.api.estoreapi.controller;
 
+import com.estore.api.estoreapi.persistence.ShoppingCartDao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ShoppingCartControllerTest {
+
+    private ShoppingCartController shoppingCartController;
+    private ShoppingCartDao mockShoppingCartDao;
+    private InventoryController mockInventoryController;
+    
+    @BeforeEach
+    public void setupShoppingCartController(){
+        mockShoppingCartDao = mock(ShoppingCartDao.class);
+        mockInventoryController = mock(InventoryController.class);
+        shoppingCartController = new ShoppingCartController(mockShoppingCartDao, mockInventoryController); 
+    }
     
 }
