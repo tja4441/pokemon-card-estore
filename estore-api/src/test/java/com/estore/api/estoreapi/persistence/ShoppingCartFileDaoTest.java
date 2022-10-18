d0bd8576acf8d10e1bbfe313e96514b67b9047ff
tja4441 <tja4441@g.rit.edu>
2022-10-18 19:07:41 -0400

    Almost done with tests, but I am braindead. - Timothy Avila

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/ShoppingCartFileDaoTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/ShoppingCartFileDaoTest.java
@@ -16,7 +16,9 @@ import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Io;
 
+import com.estore.api.estoreapi.controller.InventoryController;
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.model.ShoppingCart;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,7 +33,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 public class ShoppingCartFileDaoTest {
     ShoppingCartFileDao shoppingCartFileDao;
     ShoppingCart[] testShoppingCarts;
+    Product[] testProducts;
     ObjectMapper mockObjectMapper;
+    InventoryController mockInventoryController;
 
     /**
      * Before each test, we will create and inject a Mock Object Mapper to
@@ -41,12 +45,19 @@ public class ShoppingCartFileDaoTest {
     @BeforeEach
     public void setupShoppingCartFileDao() throws IOException {
         mockObjectMapper = mock(ObjectMapper.class);
+        mockInventoryController = mock(InventoryController.class);
+
+        testProducts = new Product[1];
+        testProducts[0] = new Product(2,"Greninja",20,200.00f);
 
         ShoppingCart testShoppingCart = new ShoppingCart(1);
         testShoppingCart.addToCart(new Product(1, "Greninja", 20, 200.00f));
         testShoppingCarts = new ShoppingCart[1];
         testShoppingCarts[0] = testShoppingCart;
         
+        when(mockObjectMapper.readValue(new File("Charmander_Is_Better.txt"),Product[].class)).thenReturn(testProducts);
+        when(mockObjectMapper.enable(SerializationFeature.INDENT_OUTPUT)).thenReturn(mockObjectMapper);
+
         when(mockObjectMapper.readValue(new File("filename.txt"),ShoppingCart[].class)).thenReturn(testShoppingCarts);
         when(mockObjectMapper.enable(SerializationFeature.INDENT_OUTPUT)).thenReturn(mockObjectMapper);
         shoppingCartFileDao = new ShoppingCartFileDao("filename.txt", mockObjectMapper);
@@ -90,18 +101,49 @@ public class ShoppingCartFileDaoTest {
 
     @Test
     public void testDeleteCart() {
+        // Invoke
+        boolean result = assertDoesNotThrow(() -> shoppingCartFileDao.deleteCart(1),
+                                                "Unexpected exception was thrown.");
+
+        // Analyze
+        assertEquals(result, true);
+        assertEquals(shoppingCartFileDao.carts.size(), testShoppingCarts.length - 1);
     }
 
     @Test
-    public void testAddToCart() {
+    public void testAddToCart() throws IOException {
+        // Invoke
+        ShoppingCart cart = new ShoppingCart(1);
+        cart = shoppingCartFileDao.addToCart(1, new Product(1, "Greninja", 20, 200.00f));
+        ShoppingCart nullResult = shoppingCartFileDao.addToCart(2, new Product(1, "Greninja", 20, 200.00f));
+        // Analyze
+        assertEquals(testShoppingCarts[0], cart);
+        assertNull(nullResult);
     }
 
     @Test
-    public void testDeleteFromCart() {
+    public void testDeleteFromCart() throws IOException {
+        // Invoke
+        ShoppingCart cart = testShoppingCarts[0];
+        cart = shoppingCartFileDao.deleteFromCart(1, new Product(1, "Greninja", 20, 200.00f));
+        ShoppingCart nullResult = shoppingCartFileDao.deleteFromCart(2, new Product(1, "Greninja", 20, 200.00f));
+        // Analyze
+        assertEquals(new ShoppingCart(1), cart);
+        assertNull(nullResult);
     }
 
     @Test
-    public void testRefreshCart() {
+    public void testRefreshCart() throws IOException {
+        // Invoke
+        ShoppingCart cart1 = shoppingCartFileDao.createCart(new ShoppingCart(2));
+        shoppingCartFileDao.addToCart(2, new Product(1, "Greninja", 25, 149.99f));
+        shoppingCartFileDao.refreshCart(2, mockInventoryController);
+        ShoppingCart cart2 = shoppingCartFileDao.createCart(new ShoppingCart(3));
+        shoppingCartFileDao.addToCart(3, new Product(2, "Victini", 5, 29.50f));
+        shoppingCartFileDao.updateCart(cart2);
+        // Analyze
+        assertEquals(cart1, testShoppingCarts[0]);
+        assertEquals(cart2, new ShoppingCart(3));
     }
 
     @Test
