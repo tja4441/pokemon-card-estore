3e6d5e63f90098d42230bf5062857a104e4d16e8
zach <zw7571@g.rit.edu>
2022-10-30 19:52:27 -0400

    AddToCart button is now on each product page and card!

--- estore-ui/src/app/add-to-cart/add-to-cart.component.ts
+++ estore-ui/src/app/add-to-cart/add-to-cart.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddToCartComponent implements OnInit {
 
-  constructor() { }
+  @Input() product!: Product
+  @Output() shoppingCart = new EventEmitter()
+  invProduct!: Product
+
+  
+  constructor(private userService: UserService,
+    private cartService: ShoppingCartService,
+    private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProduct()
   }
 
+  addToCart(): void{
+    this.cartService.addToCart(this.userService.getUser().id, this.invProduct )
+    .subscribe(shoppingCart => this.shoppingCart.emit())
+  }
+
+  getProduct(){
+    this.productService.getProduct(this.product.id).subscribe(
+    invproduct => this.invProduct = invproduct)
+  }
 }
