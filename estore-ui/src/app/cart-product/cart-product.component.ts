dd3a81ae6b648b18cd4a7d60d966619f6af81055
zach <zw7571@g.rit.edu>
2022-10-30 17:28:22 -0400

    Api now responds to AddToCart via UI button

--- estore-ui/src/app/cart-product/cart-product.component.ts
+++ estore-ui/src/app/cart-product/cart-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { ShoppingCart } from '../ShoppingCart';
 import { UserService } from '../user.service';
@@ -13,17 +14,25 @@ export class CartProductComponent implements OnInit {
 
   @Input() product!: Product
   @Output() shoppingCartAdd = new EventEmitter()
+  invProduct!: Product
 
   constructor(private userService: UserService,
-    private cartService: ShoppingCartService) { }
+    private cartService: ShoppingCartService,
+    private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProduct()
   }
 
 
   addToCart(): void{
-    this.cartService.addToCart(this.userService.getUser().id, this.product)
+    this.cartService.addToCart(this.userService.getUser().id, this.invProduct )
     .subscribe(shoppingCart => this.shoppingCartAdd.emit())
+  }
+
+  getProduct(){
+    this.productService.getProduct(this.product.id).subscribe(
+    invproduct => this.invProduct = invproduct)
   }
 
 }
