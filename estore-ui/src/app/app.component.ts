072007a25537c2e6d78fab64ce920539def97988
zach <zw7571@g.rit.edu>
2022-11-03 23:59:00 -0400

    Got rid of un-needed imports

--- estore-ui/src/app/app.component.ts
+++ estore-ui/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
-import { ThisReceiver } from '@angular/compiler';
-import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-root',
