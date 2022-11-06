aaf38a6ef2fbfd23a040cfdc5a655cb1fb85a56f
jderosier <jld3877@rit.edu>
2022-11-06 13:36:39 -0500

    removed logger

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartFileDao.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartFileDao.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -30,7 +29,6 @@ public class ShoppingCartFileDao implements ShoppingCartDao {
     private ObjectMapper orderMapper;
     private String cartFilename;                    // Filename to read/write
     private String orderFilename;
-    private static final Logger LOG = Logger.getLogger(ShoppingCartFileDao.class.getName());
 
     /**
      * Creates a {@linkplain ShoppingCart cart} File Data Access Object
