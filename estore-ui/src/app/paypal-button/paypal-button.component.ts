d93e180478842844184657848f1d6535094db428
zach <zw7571@g.rit.edu>
2022-11-07 18:14:19 -0500

    Trying to figureout onClick with paypal integration!

--- estore-ui/src/app/paypal-button/paypal-button.component.ts
+++ estore-ui/src/app/paypal-button/paypal-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { ShoppingCart } from '../ShoppingCart';
 import { UserService } from '../user.service';
@@ -14,15 +15,25 @@ export class PaypalButtonComponent implements OnInit {
 
   @Input() order!: ShoppingCart
 
+  canCheckOut = true;
+
   @ViewChild('paypal', {static: true}) paypalElement!: ElementRef
 
   constructor(private cartService: ShoppingCartService,
-              private userService: UserService) { }
-
+              private userService: UserService,
+              private route: Router) { }
+  
   ngOnInit(): void {
     const self = this;
     paypal
     .Buttons({
+      onClick: () => {
+        self.cartService.refreshCart(self.userService.getId())
+      .subscribe(boolean => self.canCheckOut = boolean)
+      if(!this.canCheckOut){
+        self.route.navigate([''])
+      }
+      },
       createOrder: (data: any, actions: any) => {
         return actions.order.create({
           purchase_units: [
@@ -35,16 +46,16 @@ export class PaypalButtonComponent implements OnInit {
           ],
         });
       },
-
       onApprove: async (data: any, actions: any) => {
         const payment = await actions.order.capture();
         self.cartService.checkout(self.order.id)
-          .subscribe(shoppingCart => this.order = shoppingCart)
-         
-      },
+          .subscribe(shoppingCart => self.order = shoppingCart)
 
+        },
+      
       onError: (err: any) => {
         alert('Payment Failed')
+        self.route.navigate([''])
       },
     })
     .render(this.paypalElement.nativeElement);
