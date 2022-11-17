db59917ae55e20bfb2023194afd672f55c04e2eb
zach <zw7571@g.rit.edu>
2022-11-17 17:25:37 -0500

    Added Inventory to the store!

--- estore-ui/src/app/products/product-card/product-card.component.ts
+++ estore-ui/src/app/products/product-card/product-card.component.ts
@@ -3,19 +3,25 @@ import { ImageService } from 'src/app/services/image.service';
 import { Product } from '../../model/product';
 import { UserService } from '../../services/user.service';
 
+
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-  @Input() card: Product | undefined;
+  @Input() card!: Product
   @Output() deletedItemEvent = new EventEmitter<number>()
+
   public turned: boolean = false;
   public source: String | undefined = undefined;
   public editing: boolean = false;
+  image!: File 
+
   constructor(private userService: UserService,
-              private imageService: ImageService) { }
+              private imageService: ImageService) {
+               }
 
   ngOnInit(): void {
     if (this.card != undefined) {
@@ -54,6 +60,21 @@ export class ProductCardComponent implements OnInit {
    */
   deleteItem(id: number) {
     this.deletedItemEvent.emit(id);
+  }
+
+  onFileChange(event: any): void {
+    this.image = event.target.files[0];
+  }
+
+  uploadImage(): void {
+
+    let name = this.card.name;
+
+    name = name + ".png";
+    if (this.image) {
+      const image = new File([this.image], name, {type: this.image.type});
+      this.imageService.uploadImage(image).subscribe(_ => window.location.reload())
+    }
   }
 
   turn(){
